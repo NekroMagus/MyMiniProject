a41b34ac1f8b01b5925fb4646f88603268079942
NekroMagus <nekromagus@mail.ru>
2019-07-27 15:52:03 +0300

    Files create

---
+++ SimpleSpringSecurityCrudApp/src/main/java/com/springsecurity/dao/DAO.java
@@ -0,0 +1,16 @@
+package com.springsecurity.dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+
+    void insert(T t);
+
+    void update(T t);
+
+    void delete(Long id);
+
+    T findById(Long id);
+
+    List<T> findAll();
+}
