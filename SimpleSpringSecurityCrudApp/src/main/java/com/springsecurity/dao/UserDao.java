2d51d53c3275e47f3fecb6ed877b853ab890b6ad
NekroMagus <nekromagus@mail.ru>
2019-08-08 18:22:25 +0300

    deleted

--- SimpleSpringSecurityCrudApp/src/main/java/com/springsecurity/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.springsecurity.dao;
-
-import com.springsecurity.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User,Long> {
-}
--- SimpleSpringSecurityCrudApp/src/main/java/com/springsecurity/model/Role.java
+++
@@ -1,5 +0,0 @@
-package com.springsecurity.model;
-
-public enum Role {
-    USER, ADMIN, ANONYMOUS;
-}
