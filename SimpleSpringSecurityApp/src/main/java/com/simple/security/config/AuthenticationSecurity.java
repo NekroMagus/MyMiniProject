841f2dc2850b573e35a3cc2c1c216ecb262cabc4
NekroMagus <nekromagus@mail.ru>
2019-08-25 00:35:19 +0300

    deleted imports

--- SimpleSpringSecurityApp/src/main/java/com/simple/security/config/AuthenticationSecurity.java
+++ SimpleSpringSecurityApp/src/main/java/com/simple/security/config/AuthenticationSecurity.java
@@ -4,10 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
