74d13d59a798f9cd5e1b41faaa3bb8ea80632025
NekroMagus <Nekromagus@mail.ru>
2019-09-16 23:07:16 +0300

    new edits

--- BootstrapSpringBootApp/src/main/java/com/bootstrap/springboot/domain/Role.java
+++ BootstrapSpringBootApp/src/main/java/com/bootstrap/springboot/domain/Role.java
@@ -22,10 +22,26 @@ public class Role implements GrantedAuthority {
     private long id;
 
     @Column(name ="role")
-    private String role;
+    private String name;
 
     @Override
     public String getAuthority() {
-        return role;
+        return name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
