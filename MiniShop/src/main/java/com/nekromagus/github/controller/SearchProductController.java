7ecff7a519117530ed33d471bfa012db98c15de8
NekroMagus <NekroMagus@mail.ru>
2019-10-31 12:43:55 +0300

    add comments

--- MiniShop/src/main/java/com/nekromagus/github/controller/SearchProductController.java
+++ MiniShop/src/main/java/com/nekromagus/github/controller/SearchProductController.java
@@ -19,42 +19,53 @@ public class SearchProductController {
 
     private List<JsonResponseProduct> resp;
 
+    /*
+        Поиск всех продуктов
+     */
     @PostMapping("/all")
     public List<JsonResponseProduct> getAllProducts() {
         resp = new ArrayList<>();
         List<Product> products = dao.findAll();
-        for(Product p: products){
-            resp.add(new JsonResponseProduct(p.getPrice(),p.getModel(),p.getSeller().getPhone()));
+        for (Product p : products) {
+            resp.add(new JsonResponseProduct(p.getPrice(), p.getModel(), p.getSeller().getPhone()));
         }
         return resp;
     }
 
+    /*
+           Поиск по максимальной цене
+    */
     @PostMapping("/maxPrice")
     public List<JsonResponseProduct> getProductsWithMaxPrice(@RequestBody JsonSearchCriteria criteria) {
         resp = new ArrayList<>();
         List<Product> products = dao.findProductsByPriceLessThanEqual(criteria.getMaxPrice());
-        for(Product p: products){
-            resp.add(new JsonResponseProduct(p.getPrice(),p.getModel(),p.getSeller().getPhone()));
+        for (Product p : products) {
+            resp.add(new JsonResponseProduct(p.getPrice(), p.getModel(), p.getSeller().getPhone()));
         }
         return resp;
     }
 
+    /*
+           Поиск по минимальной цене
+    */
     @PostMapping("/minPrice")
-    public List<JsonResponseProduct> getProductsWithMinPrice(@RequestBody JsonSearchCriteria criteria ){
+    public List<JsonResponseProduct> getProductsWithMinPrice(@RequestBody JsonSearchCriteria criteria) {
         resp = new ArrayList<>();
         List<Product> products = dao.findProductsByPriceGreaterThanEqual(criteria.getMinPrice());
-        for(Product p: products){
-            resp.add(new JsonResponseProduct(p.getPrice(),p.getModel(),p.getSeller().getPhone()));
+        for (Product p : products) {
+            resp.add(new JsonResponseProduct(p.getPrice(), p.getModel(), p.getSeller().getPhone()));
         }
         return resp;
     }
-
+    /*
+           Поиск в пределах минимальной и максимального значения
+    */
     @PostMapping("/betweenPrice")
-    public List<JsonResponseProduct> getProductsBetweenPrice(@RequestBody JsonSearchCriteria criteria ) {
+    public List<JsonResponseProduct> getProductsBetweenPrice(@RequestBody JsonSearchCriteria criteria) {
         resp = new ArrayList<>();
-        List<Product> products = dao.findProductsByPriceBetween(criteria.getMinPrice(),criteria.getMaxPrice());
-        for(Product p: products){
-            resp.add(new JsonResponseProduct(p.getPrice(),p.getModel(),p.getSeller().getPhone()));
+        List<Product> products = dao.findProductsByPriceBetween(criteria.getMinPrice(), criteria.getMaxPrice());
+        for (Product p : products) {
+            resp.add(new JsonResponseProduct(p.getPrice(), p.getModel(), p.getSeller().getPhone()));
         }
         return resp;
     }
