18eb967332c4274235bc947e2ff7662efebe6ab8
NekroMagus <NekroMagus@mail.ru>
2019-11-03 17:25:51 +0300

    add https support with my ssl(for test)

--- MiniShop/src/main/java/com/nekromagus/github/config/MvcConfigurer.java
+++ MiniShop/src/main/java/com/nekromagus/github/config/MvcConfigurer.java
@@ -1,8 +1,15 @@
 package com.nekromagus.github.config;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import sun.security.util.SecurityConstants;
 
 @Configuration
 public class MvcConfigurer implements WebMvcConfigurer {
@@ -10,5 +17,33 @@ public class MvcConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:static/");
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addAdditionalTomcatConnectors(httpConnector());
+        factory.addContextCustomizers(securityCustomizer());
+        return factory;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    private TomcatContextCustomizer securityCustomizer() {
+        return context -> {
+            SecurityConstraint securityConstraint = new SecurityConstraint();
+            securityConstraint.setUserConstraint("CONFIDENTIAL");
+            SecurityCollection collection = new SecurityCollection();
+            collection.addPattern("/*");
+            securityConstraint.addCollection(collection);
+            context.addConstraint(securityConstraint);
+        };
     }
 }
